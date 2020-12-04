d4b70bccac26ef14577412bd3bd69d6e6407b528
ahiemann <hiemann.andreas@gmail.com>
2020-12-04 19:12:07 +0100

    Removed parts from development that aren't used anymore

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,7 +19,6 @@ import scala.util.{Failure, Success}
 object Main extends App {
 
   val directory = "./testdata"
-  var sinkData = Vector[String]()
 
   val fs = FileSystems.getDefault
 
@@ -32,11 +31,6 @@ object Main extends App {
 
   val filterFilesFlow:Flow[Path, Path, NotUsed] = Flow[Path].filter(p => Files.isRegularFile(p))
 
-  val readFileFlow:Flow[Path, ByteString, NotUsed] = Flow[Path].flatMapConcat(path => {
-    FileIO.fromPath(path).reduce((a, b) => a ++ b)
-  })
-
-
   val extractFulltextFlow:Flow[Path, String, NotUsed] = Flow.fromFunction((path:Path) => {
     val tika = new Tika()
     tika.setMaxStringLength(Int.MaxValue)
@@ -44,23 +38,7 @@ object Main extends App {
   })
 
 
-  // val consoleSink2:Sink[String, Future[Done]] = Sink.collection
-
   val result = fileSource.via(filterFilesFlow).via(extractFulltextFlow).runWith(Sink.seq[String])
   val fresult = Await.result(result, 30 seconds)
   println(fresult.size)
-
-/*
-  val numbers = 1 to 1000
-  //We create a Source that will iterate over the number sequence
-  val numberSource: Source[Int, NotUsed] = Source.fromIterator(() => numbers.iterator)
-  //Only let pass even numbers through the Flow
-  val isEvenFlow: Flow[Int, Int, NotUsed] = Flow[Int].filter((num) => num % 2 == 0)
-  //Create a Source of even random numbers by combining the random number Source with the even number filter Flow
-  val evenNumbersSource: Source[Int, NotUsed] = numberSource.via(isEvenFlow)
-  //A Sink that will write its input onto the console
-  val consoleSink: Sink[Int, Future[Done]] = Sink.foreach[Int](println)
-  //Connect the Source with the Sink and run it using the materializer
-  evenNumbersSource.runWith(consoleSink)
-*/
 }
