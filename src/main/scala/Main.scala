dc37784b70cbf6d26b934c5ea46ce50c7342032c
ahiemann <hiemann.andreas@gmail.com>
2020-12-19 22:12:07 +0100

    Work on Spark integration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,10 @@ import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
 import kafka.DocInformationsDeserializer
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.tika.metadata.Metadata
@@ -30,23 +31,26 @@ object Main extends App {
   RunnableGraph.fromGraph(graph).run()
 
   // Kafka
-  //var consumer = new Consumer()
-
   /*
+  var consumer = new Consumer()
+
+
   while(true) {
     println("Polling...")
     val records = consumer.consumer.poll(Duration.ofMillis(100))
     for (record <- records.asScala) {
       println(record)
     }
-  }
-  */
+  }*/
 
    // Spark
+   val sparkConfig = new SparkConf().setMaster("local[*]").setAppName("FileInformationStream")
+   val sc = new StreamingContext(sparkConfig, Seconds(1))
+    sc.sparkContext.setLogLevel("ERROR")
 
-  val spark = SparkSession.builder.appName("Spark Streaming").config("spark.master", "local").getOrCreate()
 
-  val scalaKafkaParams = Map[String, Object](
+
+  val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "localhost:9092",
     "key.deserializer" -> classOf[StringDeserializer],
     "value.deserializer" -> classOf[DocInformationsDeserializer],
@@ -54,30 +58,51 @@ object Main extends App {
     "auto.offset.reset" -> "latest",
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
-  val kafkaParams=scalaKafkaParams.asJava
-  val sc = new StreamingContext(spark.sparkContext, Seconds(1))
 
-  val topics = asJavaCollection(Array("extraction"))
-  val offset = Map[TopicPartition, java.lang.Long]((new TopicPartition("data",1),1L)).asJava
 
-  val kafkaStream = KafkaUtils.createDirectStream[String, String](sc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+
+  val topics = Array("extraction")
+
+  val kafkaStream = KafkaUtils.createDirectStream[String, (String, Metadata)](
+    sc,
+    LocationStrategies.PreferConsistent,
+    ConsumerStrategies.Subscribe[String, (String, Metadata)](topics, kafkaParams)
+  )
 
   val docDataStream = kafkaStream.map(record => record.value)
   val docDataStream1Second = docDataStream.window(Seconds(1))
 
-  docDataStream1Second.foreachRDD(rdd =>
+  val dataStream = docDataStream1Second.map(record => {
+    val metadata = record._2
+    (record._1, metadata.get("resourceName"), metadata.get("Author"), metadata.get("date"), metadata.get("dc:format"))
+  })
+
+  dataStream.foreachRDD { rdd =>
+    val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+    import spark.implicits._
+    // implicit val myObjEncoder = org.apache.spark.sql.Encoders.kryo[Metadata]
+
     if (rdd.isEmpty()) {
       println("Keine neuen Daten")
     }
     else {
       println("neue Daten")
+      val df = rdd.toDF()
+      df.createOrReplaceTempView("DocInformationDataFrame")
+
+      val countDataFrame = spark.sql("select count(*) from DocInformationDataFrame")
+      countDataFrame.show()
+
+      val completeDataFrame = spark.sql("select * from DocInformationDataFrame")
+      completeDataFrame.show()
     }
-  )
+    //spark.stop()
+  }
   // stream.foreachRDD(rdd => println( rdd.name))
 
   sc.start()
   sc.awaitTermination()
-  spark.stop()
+
 
 
 
